6971920651bc92ac044ce806150465e940f028ce
maddocash <danksashleigh@gmail.com>
2017-10-09 21:04:02 +0100

    es6 update and added models folder

--- app.js
+++ app.js
@@ -1,52 +1,59 @@
 // 3rd party modules
 require(`dotenv`).config();
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require(`express`);
+const path = require(`path`);
+const favicon = require(`serve-favicon`);
+const logger = require(`morgan`);
+const cookieParser = require(`cookie-parser`);
+const bodyParser = require(`body-parser`);
+// const mongoose = require(`mongoose`); // uncomment for mongoose
 
 // my modules
-const config = require(`./config/config.js`)
-var index = require('./routes/index');
-var users = require('./routes/users');
+const config = require(`./config/config.js`);
+const index = require(`./routes/index`);
+const users = require(`./routes/users`);
 
-mongoose.connect(`${config.dbProtocol}${config.dbUser}:${config.dbPass}@${config.dbHost}:${config.dbPort}/${config.dbName}`, { useMongoClient: true });
+// connect to database
+// uncomment for mongoose
+// mongoose.Promise = global.Promise;
+// mongoose.connect(`${config.dbProtocol}${config.dbUser}:${config.dbPass}@${config.dbHost}:${config.dbPort}/${config.dbName}`, (err, database) => {
+//   db = database;
+//   return err ? console.log(err) : console.log(`connected to ${db}`)
+// });
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set(`views`, path.join(__dirname, `views`));
+app.set(`view engine`, `hbs`);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev')); // this is to help with logging
+//app.use(favicon(path.join(__dirname, `public`, `favicon.ico`)));
+app.use(logger(`dev`)); // this is to help with logging
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser()); //helps you do stuff across sessions
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, `public`)));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(`/`, index);
+app.use(`/users`, users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  let err = new Error(`Not Found`);
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get(`env`) === `development` ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render(`error`);
 });
 
 module.exports = app;
